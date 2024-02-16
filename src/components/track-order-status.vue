8d1a23e3ba24ac7d6b9283bce08ef59fe739c2ad
Aniket Bhatnagar <101235638+Aniket8448@users.noreply.github.com>
2024-02-16 18:42:08 +0530

    Added Logs

--- src/components/track-order-status.vue
+++ src/components/track-order-status.vue
@@ -12,21 +12,27 @@
                 id="ordernumber"
                 class="p-4 block w-full rounded-md border-0 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder="343453432"
-                v-model="orderNumber" required/>
+                v-model="orderNumber"
+                required />
           </div>
         </div>
         <div class="mt-4" v-if="emailValidationStatus">
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
           <div class="mt-2">
-            <input type="email" name="email" id="email"
-                   class="p-4 block w-full rounded-md border-0 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                   placeholder="you@example.com" v-model="email" required/>
+            <input
+                type="email"
+                name="email"
+                id="email"
+                class="p-4 block w-full rounded-md border-0 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="you@example.com"
+                v-model="email"
+                required />
           </div>
         </div>
-        </div>
-        <input type="submit"
-               class="cursor-pointer mt-8 rounded-md bg-indigo-600 px-3.5 py-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-               value="Track Order"/>
+        <input
+            type="submit"
+            class="cursor-pointer mt-8 rounded-md bg-indigo-600 px-3.5 py-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            value="Track Order" />
       </form>
       <p v-if="store.wrongCreds" class="text-center text-red-500">Invalid details provided.</p>
     </div>
@@ -40,24 +46,25 @@
       </div>
     </div>
     <div class="container max-w-6xl m-auto my-20 px-5 lg:px-0" v-if="trackingDetails?.length > 0 && hasQuery">
-      <div class="grid  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-10 lg:gap-y-0">
-        <!-- Grid One  -->
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-10 lg:gap-y-0">
+        <!-- Grid One -->
         <div class="overflow-hidden bg-white rounded-lg shadow">
           <div class="border-b border-gray-200 bg-white px-4 py-5 px-6">
             <h3 class="text-base font-semibold leading-6 text-gray-900">Estimated Delivery</h3>
           </div>
           <ul>
-            <li class="text-indigo-600 text-4xl border-b border-gray-200 p-10 px-6">{{
-                AppHelper.formatDate(trackingData?.deliveredTime)
-              }}
+            <li class="text-indigo-600 text-4xl border-b border-gray-200 p-10 px-6">
+              {{ AppHelper.formatDate(trackingData?.deliveredTime) }}
             </li>
             <li class="border-b border-gray-200 p-6 px-6">
               <div class="pb-6">
                 <span>Current Location: </span>
-                <span class="text-gray-600" v-if="trackingData?.transitStatus === 'DELIVERED'"> {{
-                    trackingDetails[0].address
-                  }},{{ trackingData?.shipTo }} </span>
-                <span class="text-gray-600" v-else> {{ trackingData?.shipFrom ?? trackingDetails[0]?.address }} </span>
+                <span class="text-gray-600" v-if="trackingData?.transitStatus === 'DELIVERED'">
+                  {{ trackingDetails[0].address }}, {{ trackingData?.shipTo }}
+                </span>
+                <span class="text-gray-600" v-else>
+                  {{ trackingData?.shipFrom ?? trackingDetails[0]?.address }}
+                </span>
               </div>
               <div>
                 <span>Destination: </span>
@@ -69,59 +76,56 @@
                 <p>Tracking Number:</p>
                 <p class="text-indigo-600">
                   <img class="h-10 inline"
-                       src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/125_Fedex_logo_logos-512.png"/>
+                       src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/125_Fedex_logo_logos-512.png" />
                   #{{ trackingData?.trackNo }}
                 </p>
               </div>
             </li>
             <li class="p-6 px-6">
               <div class="flex flex-col gap-6">
-                <div class="flex flex-row  justify-between items-center">
-                  <a class="text-indigo-600" :href="'tel:' + contactInfo?.phone">{{contactInfo?.phone}}</a>
+                <div class="flex flex-row justify-between items-center">
+                  <a class="text-indigo-600" :href="'tel:' + contactInfo?.phone">{{ contactInfo?.phone }}</a>
                   <a href="#">
                     <PhoneIcon
                         class="h-4 w-4 outline outline-1 outline-gray-300 outline-offset-4 rounded-full text-indigo-600"
-                        aria-hidden="true"/>
+                        aria-hidden="true" />
                   </a>
                 </div>
                 <div class="flex flex-row justify-between items-center">
-                  <a class="text-indigo-600" :href="'mailto:' + contactInfo?.email">{{contactInfo?.email}}</a>
+                  <a class="text-indigo-600" :href="'mailto:' + contactInfo?.email">{{ contactInfo?.email }}</a>
                   <a href="#">
                     <EnvelopeIcon
                         class="h-4 w-4 outline outline-1 outline-gray-300 outline-offset-4 rounded-full text-indigo-600"
-                        aria-hidden="true"/>
+                        aria-hidden="true" />
                   </a>
                 </div>
                 <div class="flex flex-row justify-between items-center">
-                  <a class="text-indigo-600" :href=contactInfo?.policy target="_blank">Return Policy</a>
+                  <a class="text-indigo-600" :href="contactInfo?.policy" target="_blank">Return Policy</a>
                   <a href="#">
                     <ReceiptRefundIcon
                         class="h-4 w-4 outline outline-1 outline-gray-300 outline-offset-4 rounded-full text-indigo-600"
-                        aria-hidden="true"/>
+                        aria-hidden="true" />
                   </a>
                 </div>
               </div>
             </li>
           </ul>
         </div>
-        <!-- Grid Two  -->
+        <!-- Grid Two -->
         <div class="overflow-hidden bg-white rounded-lg shadow">
           <div class="border-b border-gray-200 bg-white px-4 py-5 px-6">
             <h3 class="text-base font-semibold leading-6 text-gray-900">Shipment Progress</h3>
           </div>
-
           <div>
             <div v-if="trackingDetails && trackingDetails.length > 0" class="h-[610px] overflow-auto">
-              <!-- Existing loop over trackingDetails -->
               <div v-for="(detail, index) in trackingDetails" :key="index"
                    class="pb-6 relative leading-7 flex gap-4 border-b border-gray-200 p-6 px-6 max-h-[80vh] overflow-auto">
                 <div :class="[
-        'dot mt-[10px]',
-        isDelivered ? 'outline outline-4 outline-green-200 bg-green-600' :
-        (trackingData?.lastTrackingTime === detail.eventTime) ? 'outline outline-4 outline-indigo-200 bg-indigo-600' :
-        'bg-gray-400'
-      ]"></div>
-
+                  'dot mt-[10px]',
+                  isDelivered ? 'outline outline-4 outline-green-200 bg-green-600' :
+                  (trackingData?.lastTrackingTime === detail.eventTime) ? 'outline outline-4 outline-indigo-200 bg-indigo-600' :
+                  'bg-gray-400'
+                ]"></div>
                 <div>
                   <p class="font-medium">{{ detail?.eventTime }}</p>
                   <p class="text-gray-600 font-light text-sm">{{ detail?.eventDetail }}</p>
@@ -130,29 +134,27 @@
               </div>
             </div>
           </div>
-
         </div>
-        <!-- Grid Three  -->
+        <!-- Grid Three -->
         <div class="overflow-hidden bg-white rounded-lg shadow">
           <div class="border-b border-gray-200 bg-white px-4 py-5 px-6">
             <h3 class="text-base font-semibold leading-6 text-gray-900 text-uppercase">Recommended</h3>
           </div>
           <ul>
             <li>
-              <div class="mt-6  grid grid-rows-1 px-6">
+              <div class="mt-6 grid grid-rows-1 px-6">
                 <div v-for="product in upsellingProducts" :key="product.id"
                      class="group relative grid grid-cols-3 pb-6 gap-x-10">
-                  <div
-                      class="w-24 overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-20 xl:h-24">
+                  <div class="w-24 overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-20 xl:h-24">
                     <img :src="product.image"
-                         class="h-full w-full object-cover object-center"/>
+                         class="h-full w-full object-cover object-center" />
                   </div>
                   <div class="col-span-2">
                     <h3 class="text-sm text-gray-700">
-                      <span class="absolute inset-0"/>
+                      <span class="absolute inset-0"></span>
                       {{ product.name }}
                     </h3>
-                    <p class="mt-1 text-sm font-medium text-gray-900">{{contactInfo?.currency}} {{ product.price }}</p>
+                    <p class="mt-1 text-sm font-medium text-gray-900">{{ contactInfo?.currency }} {{ product.price }}</p>
                   </div>
                 </div>
               </div>
@@ -161,13 +163,9 @@
         </div>
       </div>
       <div class="text-sm text-gray-600 mt-4">
-        <a
-        >Powered By
-          <img
-              src="../assets/full-logo-track-order.png"
-              alt="logo"
-              class="ml-2 h-6 inline"
-          /></a>
+        <a>Powered By
+          <img src="../assets/full-logo-track-order.png" alt="logo" class="ml-2 h-6 inline" />
+        </a>
       </div>
     </div>
 
@@ -178,6 +176,7 @@
     </div>
   </div>
 </template>
+
 
 <script setup lang="ts">
 import {PhoneIcon} from "@heroicons/vue/20/solid";
